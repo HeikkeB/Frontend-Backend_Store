7f08d82beb1441b685b8cac01407ab0fb8cc49df
Vladimir Boltin <boltinvlad@yandex.ru>
2022-11-06 21:30:36 +0300

    feat: make postgreSQL schems

--- server/db.js
+++ server/db.js
@@ -1,3 +1,12 @@
 const { Sequelize } = require('sequelize')
 
-module.exports = new Sequelize()
+module.exports = new Sequelize(
+  process.env.DB_NAME, //name database
+  process.env.DB_USER, //user database
+  process.env.DB_PASSWORD, //pass database
+  {
+    dialect: 'postgres',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+  }
+)
--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 require('dotenv').config()
 const express = require('express')
+const sequelize = require('./db')
+const models = require('./models/models')
 
 const PORT = process.env.PORT || 5001
 
 const app = express()
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const start = async () => {
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync()
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
