875d1ef4796d1a779febd3bdfc5d15224ba22edd
Vladimir Boltin <boltinvlad@yandex.ru>
2022-11-07 22:51:21 +0300

    feat: make logic server

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -7,16 +7,16 @@ class DeviceController {
   async create(req, res, next) {
     try {
       const { name, price, brandId, typeId, info } = req.body
-      const { img } = req.files
+      const { image } = req.files
       let fileName = uuid.v4() + '.jpg'
-      img.mv(path.resolve(__dirname, '..', 'static', fileName))
+      image.mv(path.resolve(__dirname, '..', 'static', fileName))
 
       const device = await Device.create({
         name,
         price,
         brandId,
         typeId,
-        img: fileName,
+        image: fileName,
       })
 
       return res.json(device)
